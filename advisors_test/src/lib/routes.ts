71cf5f924918029121b0ff174da060aaee3f5b90
Nicolas Mayorga <namayorgav@gmail.com>
2024-10-03 08:27:33 -0500

    refactor: add API routes and client routes

--- advisors_test/src/lib/routes.ts
+++ advisors_test/src/lib/routes.ts
@@ -1,4 +1,10 @@
-const baseURL = 'http://localhost:3001'
+const API_baseURL = "http://localhost:3001"
+const baseURL = "http://localhost:3000"
+
+export enum API_ROUTES {
+  ADVISORS = `${API_baseURL}/advisors`,
+}
 
 export enum ROUTES {
-    ADVISORS = `${baseURL}/advisors`}
+  ADVISORS = `${baseURL}/advisors`,
+}
