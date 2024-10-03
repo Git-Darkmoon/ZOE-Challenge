d374bedbf63a17db19032df2edfd483648298565
Nicolas Mayorga <namayorgav@gmail.com>
2024-10-02 23:58:16 -0500

    feat: add container component

---
+++ advisors_test/src/components/Container.tsx
@@ -0,0 +1,5 @@
+function Container({ children }: { children: React.ReactNode }) {
+  // This will handle some responsive styles
+  return <div className="container">{children}</div>
+}
+export default Container
