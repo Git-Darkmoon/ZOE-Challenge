3e2d894287d6202eb1d560cd0e131f6ea2886ae4
Nicolas Mayorga <namayorgav@gmail.com>
2024-10-03 11:32:12 -0500

    feat: add feature for sharing the URL with all the URLSearchParams

--- advisors_test/src/components/AdvisorsTable.tsx
+++ advisors_test/src/components/AdvisorsTable.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { API_ROUTES } from "@/lib/routes"
 import { Advisor } from "@/lib/types"
 import { formatCurrency } from "@/lib/utils"
@@ -7,20 +9,83 @@ import {
   ChevronRightIcon,
   ChevronUpIcon,
 } from "lucide-react"
-import { useRouter } from "next/navigation"
-import { SetStateAction, useEffect, useState } from "react"
+import { useRouter, useSearchParams } from "next/navigation"
+import { ChangeEvent, SetStateAction, useEffect, useState } from "react"
 
-function AdvisorsTable({ income }: { income: string }) {
+function AdvisorsTable() {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const queryIncome = searchParams.get("income")
+  const queryPage = searchParams.get("page")
+  const querySort = searchParams.get("sort")
+  const queryOrder = searchParams.get("order")
+  const queryLimit = searchParams.get("limit")
+
+  const TableHeadings: { value: keyof Advisor; label: string }[] = [
+    {
+      value: "name",
+      label: "Advisor Name",
+    },
+    {
+      value: "income",
+      label: "Income",
+    },
+  ]
+
   const [advisors, setAdvisors] = useState<Advisor[]>([])
-  const [isDescendingOrder, setIsDescendingOrder] = useState(false)
-  const [sortedColumn, setSortedColumn] = useState<keyof Advisor | null>(null)
-
-  const quantityOfUsersToBeShown = 10
-  const [currentPage, setCurrentPage] = useState(1)
+  const [isDescendingOrder, setIsDescendingOrder] = useState(
+    queryOrder === "desc"
+  )
+  const [sortedColumn, setSortedColumn] = useState<keyof Advisor | null>(
+    (querySort as keyof Advisor) || null
+  )
+  const [quantityOfUsersToBeShown, setQuantityOfUsersToBeShown] = useState(
+    Number(queryLimit) || 10
+  )
+  const [currentPage, setCurrentPage] = useState(Number(queryPage) || 1)
 
   const totalUsers = advisors?.length || 0
   const totalPages = Math.ceil(totalUsers / quantityOfUsersToBeShown)
-  const navigate = useRouter()
+
+  const updateURL = () => {
+    const params = new URLSearchParams()
+    if (queryIncome) params.set("income", queryIncome)
+    params.set("page", currentPage.toString())
+    params.set("sort", sortedColumn || "name")
+    params.set("order", isDescendingOrder ? "desc" : "asc")
+    params.set("limit", quantityOfUsersToBeShown.toString())
+    router.replace(`?${params.toString()}`)
+  }
+
+  useEffect(() => {
+    updateURL()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage, sortedColumn, isDescendingOrder, quantityOfUsersToBeShown])
+
+  useEffect(() => {
+    const getAdvisors = async () => {
+      if (!queryIncome) return
+
+      try {
+        const response = await fetch(API_ROUTES.ADVISORS)
+        const advisorsData: Advisor[] = await response.json()
+
+        const newAdvisors = advisorsData.filter((advisor) => {
+          const startingRange = Number(queryIncome) - 10_000
+          const maxRange = Number(queryIncome) + 10_000
+          return advisor.income >= startingRange && advisor.income <= maxRange
+        })
+        setAdvisors(newAdvisors)
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message)
+        }
+      }
+    }
+
+    getAdvisors()
+  }, [queryIncome])
 
   const handlePrevPage = () => {
     if (currentPage > 1) {
@@ -41,47 +106,18 @@ function AdvisorsTable({ income }: { income: string }) {
     return [...advisors].slice(startIndex, endIndex)
   }
 
+  const handleUsersDisplayedChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedOption = e.target.value
+    const amountOfUsersSelected =
+      selectedOption === "all" ? totalUsers : Number(selectedOption)
+
+    setQuantityOfUsersToBeShown(amountOfUsersSelected)
+    setCurrentPage(1) // Reset to the first page when changing the limit
+  }
+
   const toggleSortingOrder = () => {
     setIsDescendingOrder(!isDescendingOrder)
   }
-
-  const goToDetailsPage = (id: number) => {
-    navigate.push(`/advisors/${id}`)
-  }
-
-  const TableHeadings: { value: keyof Advisor; label: string }[] = [
-    {
-      value: "name",
-      label: "Advisor Name",
-    },
-    {
-      value: "income",
-      label: "Income",
-    },
-  ]
-
-  useEffect(() => {
-    const getAdvisors = async () => {
-      try {
-        const response = await fetch(API_ROUTES.ADVISORS)
-        const advisorsData: Advisor[] = await response.json()
-
-        const newAdvisors = advisorsData.filter((advisor) => {
-          const startingRange = Number(income) - 10_000
-          const maxRange = Number(income) + 10_000
-
-          return advisor.income >= startingRange && advisor.income <= maxRange
-        })
-        setAdvisors(newAdvisors)
-      } catch (error) {
-        if (error instanceof Error) {
-          console.log(error.message)
-        }
-      }
-    }
-
-    getAdvisors()
-  }, [income])
 
   const handleSort = (
     arr: Advisor[],
@@ -104,88 +140,86 @@ function AdvisorsTable({ income }: { income: string }) {
     setSortedColumn(key)
   }
 
+  const goToDetailsPage = (id: number) => {
+    router.push(`/advisors/${id}`)
+  }
+
   return (
     <>
       {advisors.length === 0 ? (
-        <h3>
-          No available Advisors based on the provided income. Please try a
-          different income value.
-        </h3>
+        <h3>No available Advisors based on the provided income.</h3>
       ) : (
         <>
           <div className="table__filter">
             <h1 className="table__results__title">
               {advisors.length} Advisors Found
             </h1>
-            <select name="" id="">
-              <option value="">All</option>
-              <option value="">Male</option>
-              <option value="">Female</option>
+            <select
+              name="displayedUsers"
+              className="table__filter__select"
+              onChange={handleUsersDisplayedChange}
+              value={
+                quantityOfUsersToBeShown === totalUsers
+                  ? "all"
+                  : quantityOfUsersToBeShown
+              }
+            >
+              <option value="5">Show 5</option>
+              <option value="10">Show 10</option>
+              <option value="all">Show all</option>
             </select>
           </div>
           <table className="table">
             <thead>
               <tr>
-                {TableHeadings.map((heading) => {
-                  return (
-                    <td className="table__heading" key={heading.value}>
-                      <button
-                        className="table__heading__wrapper"
-                        onClick={() => {
-                          handleSort(
-                            advisors,
-                            heading.value as keyof Advisor,
-                            setAdvisors,
-                            isDescendingOrder
-                          )
-                          toggleSortingOrder()
-                        }}
-                      >
-                        <strong>{heading.label}</strong>
-                        {sortedColumn === heading.value ? (
-                          isDescendingOrder ? (
-                            <div className="table__heading__wrapper__icon activeColumn">
-                              <ChevronDownIcon />
-                            </div>
-                          ) : (
-                            <div className="table__heading__wrapper__icon activeColumn">
-                              <ChevronUpIcon />
-                            </div>
-                          )
+                {TableHeadings.map((heading) => (
+                  <td className="table__heading" key={heading.value}>
+                    <button
+                      className="table__heading__wrapper"
+                      onClick={() => {
+                        handleSort(
+                          advisors,
+                          heading.value as keyof Advisor,
+                          setAdvisors,
+                          isDescendingOrder
+                        )
+                        toggleSortingOrder()
+                      }}
+                    >
+                      <p>{heading.label}</p>
+                      {sortedColumn === heading.value ? (
+                        isDescendingOrder ? (
+                          <ChevronDownIcon className="table__heading__wrapper__icon activeColumn" />
                         ) : (
-                          <div className="table__heading__wrapper__icon">
-                            <ChevronUpIcon />
-                          </div>
-                        )}
-                      </button>
-                    </td>
-                  )
-                })}
+                          <ChevronUpIcon className="table__heading__wrapper__icon activeColumn" />
+                        )
+                      ) : (
+                        <ChevronUpIcon className="table__heading__wrapper__icon" />
+                      )}
+                    </button>
+                  </td>
+                ))}
               </tr>
             </thead>
             <tbody>
-              {getUsersDisplayed().map((advisor) => {
-                const { id, name, income } = advisor
-
-                return (
-                  <tr
-                    className="table__link"
-                    key={id}
-                    onClick={() => goToDetailsPage(id)}
-                  >
-                    <td>{name}</td>
-                    <td>{formatCurrency(income.toFixed(2))}</td>
-                  </tr>
-                )
-              })}
+              {getUsersDisplayed().map((advisor) => (
+                <tr
+                  key={advisor.id}
+                  className="table__link"
+                  onClick={() => goToDetailsPage(advisor.id)}
+                >
+                  <td>{advisor.name}</td>
+                  <td>{formatCurrency(advisor.income)}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
-          {/* Table Controls */}
+
           <div className="table__controls">
             <button
               className="btn btn__ghost"
-              disabled={currentPage === 1}
               onClick={handlePrevPage}
+              disabled={currentPage === 1}
             >
               <ChevronLeftIcon />
             </button>
@@ -194,8 +228,8 @@ function AdvisorsTable({ income }: { income: string }) {
             </span>
             <button
               className="btn btn__ghost"
-              disabled={currentPage === totalPages}
               onClick={handleNextPage}
+              disabled={currentPage === totalPages}
             >
               <ChevronRightIcon />
             </button>
@@ -205,4 +239,5 @@ function AdvisorsTable({ income }: { income: string }) {
     </>
   )
 }
+
 export default AdvisorsTable
